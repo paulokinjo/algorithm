0e0536cb3fe74f812355a77f3756b7f23ca13e13
Paulo Kinjo <paulokinjo@hotmail.com>
2021-04-15 10:22:11 +0900

    first commit

---
+++ Permutations.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Permutations {
+    public static void main(String args[] ) throws Exception {
+      permutations("", "ABC");        
+    }
+
+    public static void permutations(String prefix, String suffix) {
+        if(suffix.length() == 0) System.out.println(prefix);
+        else {
+            for(int i = 0; i < suffix.length(); i++) {
+                String p = prefix + suffix.charAt(i);
+                String s = suffix.substring(0, i) + suffix.substring(i + 1, suffix.length());
+                permutations(p, s);
+            }
+        }
+    }
+}
